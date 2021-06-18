e000a25175b283bdff06834bf78a86df81bef213
Dylan <dylanwebcoder@gmail.com>
2021-06-18 16:42:43 +0200

    Project files

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
+const helmet = require('helmet');
+require('dotenv').config();
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+
+// Connexion à la base de données 
+mongoose.connect(process.env.DB_MONGO,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Lancement Express
+const app = express();
+
+// console.log(process.env.DB_USER);
+
+// Confguration CORS 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Conversion des requêtes en JSON
+app.use(bodyParser.json());
+
+//Sécuriser les headers 
+app.use(helmet());
+
+// Routes
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
+
+module.exports = app;
