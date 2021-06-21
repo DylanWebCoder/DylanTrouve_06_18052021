1f8763695fc04034252e2c3318b356e2fdecd393
DylanWebCoder <DylanWebCoder@gmail.com>
2021-06-22 00:29:22 +0200

    Crypto-js, passwordValidator et token

--- controllers/user.js
+++ controllers/user.js
@@ -1,26 +1,38 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const passwordValidator = require('password-validator');
+const cryptojs = require('crypto-js');
 
 const User = require('../models/user');
 
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: hash
-        });
-        user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
+    var schema = new passwordValidator();
+    schema.is().min(8);
+    schema.has().digits(1);
+    schema.has().uppercase(2);
+    if(schema.validate(req.body.password)){
+      bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+          const cipherText = cryptojs.HmacSHA512(req.body.email, process.env.EMAIL_KEY).toString();
+          const user = new User({
+            email: cipherText,
+            password: hash
+          });
+          user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+            .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+    }else{
+      res.status(500).json({ error : 'Le mot de passe doit comporter au moins 8 caractères, 1 nombre et 2 majuscules' })
+    };
 };
 
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    const cipherText = cryptojs.HmacSHA512(req.body.email, process.env.EMAIL_KEY).toString();
+    User.findOne({ email: cipherText })
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
@@ -34,7 +46,7 @@ exports.login = (req, res, next) => {
               userId: user._id,
               token: jwt.sign(
                 { userId: user._id },
-                'RANDOM_TOKEN_SECRET',
+                process.env.DB_TOKEN,
                 { expiresIn: '24h' }
               )
             });
